d44125f579365218b9720b2a7b013e550d2693eb
Joakim Svensson <jkm.svensson@gmail.com>
2017-11-09 13:20:32 +0100

    New

--- tests/CarTest.java
+++ tests/CarTest.java
@@ -7,80 +7,94 @@ class CarTest {
     void setTurboOn() {
         Car car1 = new Volvo240();
         Car car2 = new Saab95();
-        car1.setTurboOn();{assertFalse(car1.getTurbo());}
-        car2.setTurboOn();{assertTrue(car2.getTurbo()); }
-
+        car1.setTurboOn();
+        {
+            assertFalse(car1.getTurbo());
+        }
+        car2.setTurboOn();
+        {
+            assertTrue(car2.getTurbo());
+        }
+        car1.setTurboOn();
+        {
+            assertFalse(car1.getTurboOn());
+        }
+        car2.setTurboOn();
+        {
+            assertTrue(car2.getTurboOn());
+        }
     }
 
-    @Test
-    void setTurboOff() {
-        Car car1 = new Saab95();
-        car1.setTurboOff();
-        assertTrue(car1.getTurbo());
+        @Test
+        void setTurboOff () {
+            Car car1 = new Saab95();
+            car1.setTurboOn();
+            assertTrue(car1.getTurboOn());
+            car1.setTurboOff();
+            assertFalse(car1.getTurboOn());
+        }
+
+        @Test
+        void getTurbo () {
+        }
+
+        @Test
+        void move () {
+            Car car1 = new Saab95();
+            car1.startEngine();
+            car1.move();
+            double output = car1.getyPos();
+            assertEquals(0.1, output);
+        }
+
+        @Test
+        void turnLeft () {
+        }
+
+        @Test
+        void turnRight () {
+        }
+
+        @Test
+        void speedFactor () {
+            Car car1 = new Saab95();
+            car1.speedFactor();
+            double output = car1.speedFactor();
+            assertEquals(1.25, output);
+
+        }
+
+        @Test
+        void gas () {
+            Car car1 = new Saab95();
+            car1.gas(0.9);
+            double output = car1.getCurrentSpeed();
+            assertEquals(1.125, output);
+        }
+
+        @Test
+        void brake () {
+            Car car1 = new Volvo240();
+            car1.brake(2);
+            double output = car1.getCurrentSpeed();
+            assertEquals(0, output);
+
+        }
+
+        @Test
+        void startEngine () {
+            Car car2 = new Volvo240();
+            car2.startEngine();
+            double output = car2.getCurrentSpeed();
+            assertEquals(0.1, output);
+        }
+
+        @Test
+        void stopEngine () {
+            Car car1 = new Saab95();
+            car1.stopEngine();
+            double output = car1.getCurrentSpeed();
+            assertEquals(0, output);
+
+        }
     }
-
-    @Test
-    void getTurbo() {
-    }
-
-    @Test
-    void move() {
-        Car car1 = new Saab95();
-        car1.startEngine();
-        car1.move();
-        double output = car1.getyPos();
-        assertEquals(0.1, output);
-    }
-
-    @Test
-    void turnLeft() {
-    }
-
-    @Test
-    void turnRight() {
-    }
-
-    @Test
-    void speedFactor() {
-        Car car1 = new Saab95();
-        car1.speedFactor();
-        double output = car1.speedFactor();
-        assertEquals(1.25, output);
-
-    }
-
-    @Test
-    void gas() {
-        Car car1 = new Saab95();
-        car1.gas(0.9);
-        double output = car1.getCurrentSpeed();
-        assertEquals(1.125, output);
-    }
-
-    @Test
-    void brake() {
-        Car car1 = new Volvo240();
-        car1.brake(2);
-        double output = car1.getCurrentSpeed();
-        assertEquals(0, output);
-
-    }
-
-    @Test
-    void startEngine() {
-        Car car2 = new Volvo240();
-        car2.startEngine();
-        double output = car2.getCurrentSpeed();
-        assertEquals(0.1, output);
-    }
-
-    @Test
-    void stopEngine() {
-        Car car1 = new Saab95();
-        car1.stopEngine();
-        double output = car1.getCurrentSpeed();
-        assertEquals(0, output);
-
-    }
-
-}
