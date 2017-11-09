f0c440460c1f892c3f58d5d4b16cb133c5ec95f9
jkm.svensson@gmail.com <2wsad1wsad>
2017-11-09 06:10:39 +0100

    New

---
+++ tests/CarTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+    @Test
+    void move() {
+    }
+
+    @Test
+    void turnLeft() {
+    }
+
+    @Test
+    void turnRight() {
+    }
+
+    @Test
+    void gas() {
+    }
+
+    @Test
+    void brake() {
+    }
+
+    @Test
+    void startEngine() {
+    }
+
+    @Test
+    void stopEngine() {
+    }
+
+    @Test
+    void setTurboOn() {
+    }
+
+    @Test
+    void setTurboOff() {
+    }
+
+}
