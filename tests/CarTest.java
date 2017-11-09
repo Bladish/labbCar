cdc453d9164e1d86312182874d05b2fa3fee1c94
jkm.svensson@gmail.com <2wsad1wsad>
2017-11-09 12:13:42 +0100

    New

--- tests/CarTest.java
+++ tests/CarTest.java
@@ -3,19 +3,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
-
-    @Test
-    void speedFactor() {
-        Car car3 = new Saab95();
-        double output = car3.speedFactor();
-        assertEquals(1.25, output);
-    }
-
     @Test
     void move() {
-
     }
-
 
     @Test
     void turnLeft() {
@@ -26,19 +16,38 @@ class CarTest {
     }
 
     @Test
+    void speedFactor() {
+        Car car1 = new Saab95();
+        car1.speedFactor();
+        double output = car1.speedFactor();
+        assertEquals(1.25, output);
+
+    }
+
+    @Test
     void gas() {
     }
 
     @Test
     void brake() {
+
     }
 
     @Test
     void startEngine() {
+        Car car2 = new Volvo240();
+        car2.startEngine();
+        double output = car2.getCurrentSpeed();
+        assertEquals(0.1, output);
     }
 
     @Test
     void stopEngine() {
+        Car car1 = new Saab95();
+        car1.stopEngine();
+        double output = car1.getCurrentSpeed();
+        assertEquals(0, output);
+
     }
 
 }
